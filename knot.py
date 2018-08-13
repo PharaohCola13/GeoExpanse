bde840cab2a5b92cf085ff3ced5c873dc1a1100a
Spencer Riley <42317614+PharaohCola13@users.noreply.github.com>
2018-08-13 15:14:43 -0600

    Update knot.py

--- knot.py
+++ knot.py
@@ -8,15 +8,26 @@ from numpy import pi, linspace, cos, sin
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import writers
 
-def x_(t):
-    x = sin(t) + 2*sin(2*t)
+def x_(u,v):
+    x = sin(v) + 2 * sin(2 * v)
     return x
-def y_(t):
-    y = cos(t) - 2*cos(2*t)
+
+def y_(u,v):
+    y = cos(v) - 2 * cos(2 * v)
     return y
-def z_(t)
-    z = -sin(3*t)
+
+def z_(u,v):
+    z = -sin(u)
     return z
+
+u = linspace(0, 4 * pi, 100)
+v = linspace(0, 2 * pi, 100)
+
+u,v = np.meshgrid(u,v)
+
+x = x_(u,v)
+y = y_(u,v)
+z = z_(u,v)
 
 # Figure Properties
 fig = plt.figure(figsize=(8,8))
@@ -27,32 +38,39 @@ ax.set_facecolor('black')
 plt.axis('off')
 plt.axis('equal')
 
+# ax.set_xlim(-5,5)
+# ax.set_ylim(-5,5)
+ax.set_zlim(-5,5)
+
 
 # Surface plot
-k = ax.plot_trisurf(x, y, z, cmap='rainbow'
+k = ax.plot_surface(x, y, z, rstride=10, cstride=10, cmap='cool'
 )
+
 
 k.set_linewidth(0.0)
 k.set_edgecolor('w')
 k.set_alpha(0.5)
 
 # Defintions for animations
-def init():
-    return k,
+# def init():
+ #   return k,
 
-def animate(i):
+# def animate(i):
     # azimuth angle : 0 deg to 360 deg
     # elev = i * n --> rotates object about the xy-plane with a magnitude of n
     # azim = i * n --> rotates object around the z axis with a magnitude of n
     # For top view elev = 90
     # For side view elev = 0
 
-    ax.view_init(elev=50, azim= 4 * i)
-    return k,
+#    ax.view_init(elev=50, azim= 2 * i)
+#    return k,
+
+# Smooth transition elev = 50, frames = 180, interval = 1
 
 # Animate
-ani = FuncAnimation(fig, animate, init_func=init,
-                    frames=100, interval=20, blit=False, repeat=True)
+# ani = FuncAnimation(fig, animate, init_func=init,
+#                     frames=180, interval=1, blit=False, repeat=True)
 
 # Saving to mobius.mp4
 
@@ -60,6 +78,5 @@ ani = FuncAnimation(fig, animate, init_func=init,
 # writer = Writer(fps=15, bitrate=1800)
 
 # ani.save('mobius.mp4', writer=writer)
-
 
 plt.show()
