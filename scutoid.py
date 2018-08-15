6d3313ac25c0c04aea95a93ae065b2cccd8a9c48
Spencer Riley <42317614+PharaohCola13@users.noreply.github.com>
2018-08-15 17:25:35 -0600

    Update scutoid.py

--- scutoid.py
+++ scutoid.py
@@ -10,80 +10,79 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.animation import writers
 
 
-hexa = np.array([[-2,0,1],
-				 [-1.5, 0.866, 1],
-				 [-0.5, 0.866, 1],
-				 [0.0, 0, 1],
-				 [-0.5, -0.866, 1],
-				 [-1.5, -0.866, 1]])
+hex_top = np.array([[-2,0,1],
+		    [-1.5, 0.866, 1],
+		    [-0.5, 0.866, 1],
+		    [0.0, 0, 1],
+		    [-0.5, -0.866, 1],
+		    [-1.5, -0.866, 1]])
 
-penta = np.array([[-2,0,0],
-				  [-1.5,0.866,0],
-				  [-0.5, 0.866,0],
-				  [0.0, 0, 0],
-				  [-1, -0.866, 0]])
+pent_bottom = np.array([[-2,0,0],
+			[-1.5,0.866,0],
+			[-0.5, 0.866,0],
+			[0.0, 0, 0],
+			[-1, -0.866, 0]])
 
 cent = np.array([[-1.5, -0.866, 1],
-				 [-1, -1.266, 0.5],
-				 [-0.5, -0.866, 1]])
+		 [-1, -1.266, 0.5],
+		 [-0.5, -0.866, 1]])
 
 
-ahexa = np.array([[0,0,0],
-				 [-1, -0.866, 0],
-				 [-0.75, -1.866, 0],
-				 [0.25, -2.166, 0],
-				 [1.05, -1.466, 0],
-				 [1, -0.434, 0]])
+hex_bottom = np.array([[0,0,0],
+		       [-1, -0.866, 0],
+		       [-0.75, -1.866, 0],
+		       [0.25, -2.166, 0],
+		       [1.05, -1.466, 0],
+		       [1, -0.434, 0]])
 
-apenta = np.array([[0.0,0,1],
-				  [1, -0.434,1],
-				  [1.05, -1.466, 1],
-				  [0, -1.7, 1],
-					[-0.5,-0.866,1]])
+pent_top = np.array([[0.0,0,1],
+		     [1, -0.434,1],
+		     [1.05, -1.466, 1],
+		     [0, -1.7, 1],
+		     [-0.5,-0.866,1]])
 
-acent = np.array([[-1, -0.866, 0],
-				 [-1, -1.266, 0.5],
-				 [-0.75, -1.866, 0]])
+cent_alt = np.array([[-1, -0.866, 0],
+		     [-1, -1.266, 0.5],
+		     [-0.75, -1.866, 0]])
 
 
+# Scaling Matricies
 N = [[1,0,0],
-	 [0,1,0],
-	 [0,0,1]]
+     [0,1,0],
+     [0,0,1]]
 
-aN = [[1,0,0],
-	 [0,1,0],
-	 [0,0,1]]
+L = [[1,0,0],
+     [0,1,0],
+     [0,0,1]]
 
 M = np.zeros((6,3))
 P = np.zeros((5,3))
 Q = np.zeros((3,3))
 
-aM = np.zeros((6,3))
-aP = np.zeros((5,3))
-aQ = np.zeros((3,3))
+R = np.zeros((6,3))
+S = np.zeros((5,3))
+T = np.zeros((3,3))
 
 for i in range(6):
-	M[i,:] = np.dot(hexa[i,:],N)
+	M[i,:] = np.dot(hex_top[i,:],N)
 
 for i in range(5):
-	P[i,:] = np.dot(penta[i,:],N)
+	P[i,:] = np.dot(pent_bottom[i,:],N)
 
 for i in range(3):
 	Q[i,:] = np.dot(cent[i,:],N)
 
 
 for i in range(6):
-	aM[i,:] = np.dot(ahexa[i,:],aN)
+	R[i,:] = np.dot(hex_bottom[i,:],L)
 
 for i in range(5):
-	aP[i,:] = np.dot(apenta[i,:],aN)
+	S[i,:] = np.dot(pent_top[i,:],L)
 
 for i in range(3):
-	aQ[i,:] = np.dot(acent[i,:],aN)
-
-
-
-
+	T[i,:] = np.dot(cent_alt[i,:],L)
+	
+# Figure Properties
 fig = plt.figure(figsize=(8,8))
 ax = p3.Axes3D(fig)
 ax.set_facecolor('black')
@@ -95,48 +94,45 @@ ax.set_xlim(-2,2)
 ax.set_ylim(-2,2)
 ax.set_zlim(0,1)
 
+# Radius
 r = [-2, 2]
 
 X,Y = np.meshgrid(r,r)
 
+# Edges for the Scutoid with a Hexagon on top
 top_hex = [[M[0], M[1], M[2], M[3], M[4], M[5]]]
 
 bottom_pent = [[P[0], P[1], P[2], P[3], P[4]]]
 
 sides = [[P[0], M[0], M[1], P[1]],
-				 [P[1], M[1], M[2], P[2]],
-				 [P[2], M[2], M[3], P[3]],
-				 [P[3], P[4], Q[1], Q[2], M[4], M[3]],
-				 [P[4], Q[1], Q[0], M[5], M[0], P[0]]
-
+	 [P[1], M[1], M[2], P[2]],
+	 [P[2], M[2], M[3], P[3]],
+	 [P[3], P[4], Q[1], Q[2], M[4], M[3]],
+	 [P[4], Q[1], Q[0], M[5], M[0], P[0]]
 ]
 
 verts_cent = [[Q[0], Q[1], Q[2]]]
 
-##
+# Edges for the Scutoid with a Pentagon on top
 
-bottom_hex = [[aM[0], aM[1], aM[2], aM[3], aM[4], aM[5]]]
+bottom_hex = [[R[0], R[1], R[2], R[3], R[4], R[5]]]
 
-top_pent = [[aP[1], aP[2], aP[3], aP[4], aP[0]]]
+top_pent = [[S[1], S[2], S[3], S[4], S[0]]]
 
-alt_sides = [[aP[0], aM[0], aM[5], aP[1]],
-				[aP[1], aM[5], aM[4], aP[2]],
-				[aP[2], aM[4], aM[3], aP[3]],
-				[aP[3], aM[3], aQ[2], aQ[1], aP[4]] 
+alt_sides = [[S[0], R[0], R[5], S[1]],
+	     [S[1], R[5], R[4], S[2]],
+	     [S[2], R[4], R[3], S[3]],
+	     [S[3], R[3], T[2], T[1], S[4]]]
 
-]
+verts_acent = [[T[0], T[1], T[2], T[0]]]
 
-verts_acent = [[aQ[0], aQ[1], aQ[2], aQ[0]]]
-
-
-
+# Suface Properties (Scutoid with Hexagon on top)
 scu_top_hex = Poly3DCollection(top_hex)
 
 scu_top_hex.set_edgecolor('black')
 scu_top_hex.set_facecolor('green')
 scu_top_hex.set_linewidth(2)
 scu_top_hex.set_alpha(1)
-
 
 scu_bottom_pent = Poly3DCollection(bottom_pent)
 
@@ -159,7 +155,7 @@ scu_cent.set_linewidth(2)
 scu_cent.set_facecolor('green')
 scu_cent.set_alpha(1)
 
-##
+# Surface Properties (Scutoid with Pentagon on top)
 
 scu_bottom_hex = Poly3DCollection(bottom_hex)
 
@@ -190,12 +186,13 @@ scu_acent.set_linewidth(2)
 scu_acent.set_facecolor('yellow')
 scu_acent.set_alpha(1)
 
+# Surface Plot (Scutoid with Hexagon on top)
 sth = ax.add_collection(scu_top_hex)
 sbp = ax.add_collection(scu_bottom_pent)
 ss = ax.add_collection(scu_sides)
 sc = ax.add_collection(scu_cent)
 
-##
+# Surface Plot (Scutoid with Pentagon on top)
 
 sbh = ax.add_collection(scu_bottom_hex)
 stp = ax.add_collection(scu_top_pent)
@@ -220,14 +217,11 @@ def animate(i):
 ani = FuncAnimation(fig, animate, init_func=init,
                    frames=88, interval=1, blit=False, repeat=True)
 
-#Saving to Tesseract.mp4
+#Saving to Scutoid.mp4
 
 #Writer = writers['ffmpeg']
 #writer = Writer(fps=15, bitrate=1800)
 
-#ani.save('Tesseract.mp4', writer=writer)
-
-
-
+#ani.save('Scutoid.mp4', writer=writer)
 
 plt.show()
