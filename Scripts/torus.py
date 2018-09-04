9c92a95e5fbf5953fbfb725bb9a5e1b040ee83cc
Spencer Riley <42317614+PharaohCola13@users.noreply.github.com>
2018-09-04 16:17:41 -0600

    Update torus.py

--- Scripts/torus.py
+++ Scripts/torus.py
@@ -1,88 +1,106 @@
-# A Torus, brought to you by PharaohCola13
+# A Family of Torus, brought to you by PharaohCola13
 
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import numpy as np
-from numpy import pi, linspace, cos, sin
-from matplotlib.animation import FuncAnimation
-from matplotlib.animation import writers
 
-def x_(u, v):
-    x = 2 * ((c + a * cos(u)) * cos(v))
-    return x
+from mpl_toolkits.mplot3d.art3d import *
+from matplotlib.animation import *
+from matplotlib import *
+from numpy import *
 
-def y_(u, v):
-    y = 2 * ((c + a *cos(u)) * sin(v))
-    return y
+option = input('Run? (0) Yes, (1) No\n>> ')
 
-# look at what happens when you switch out sin(u) with sin(v)
+while option == 0:
+# Definition of x
+	def x_(u, v):
+    		x = 2 * ((c + a * cos(u)) * cos(v))
+    		return x
 
-def z_(u, v):
-    z = 2 * (a * sin(u))
-    return z
+# Definition of y
+	def y_(u, v):
+    		y = 2 * ((c + a * cos(u)) * sin(v))
+    		return y
 
-n = 100
+# Definition of z
+	def z_(u, v):
+    		z = 2 * (a * sin(u))
+    		return z
 
-# c > a produces a ring torus
-# c = a produces a horn tours
-# c < a produces a spindle torus
+# Radius 
+	radius = input('What is the radius?\n>> ')
+	c = radius
+	
+# Radius of the tube
+	radius_tube = input('What is the radius of the tube?\n>> ')
+	a = radius_tube
 
-c, a = 2, 1
+# Values of the angles
+	n = 100
+	u = linspace(0, 2 * pi, n)
+	v = linspace(0, 2 * pi, n)
 
-u = linspace(0, 2 * pi, n)
-v = linspace(0, 2 * pi, n)
+	u, v = meshgrid(u,v)
 
-u, v = np.meshgrid(u,v)
-
-x = x_(u, v)
-y = y_(u, v)
-z = z_(u, v)
+# Symbolic Representation
+	x = x_(u, v)
+	y = y_(u, v)
+	z = z_(u, v)
 
 # Figure Properties
-fig = plt.figure(figsize=(8, 8))
+	fig = plt.figure(figsize=(8,8))
 
-ax = p3.Axes3D(fig)
-ax.set_facecolor('black')
+	ax = p3.Axes3D(fig)
+	ax.set_facecolor('black') # Figure background turns black
 
-plt.axis('off')
-plt.axis('equal')
+# Axis Properties
+	plt.axis('off') # Turns off the axis grid
+	plt.axis('equal')
 
-ax.set_xlim(-5, 5)
-ax.set_ylim(-5, 5)
-ax.set_zlim(-5, 5)
+# Axis Limits
+	ax.set_xlim(-5,5)
+	ax.set_ylim(-5,5)
+	ax.set_zlim(-5,5)
 
 # Surface Plot
-t = ax.plot_surface(x, y, z, rstride=5, cstride=5, cmap='rainbow')
+	torus = ax.plot_surface(x, y, z,  rstride=5, cstride=5)
 
-t.set_edgecolor('w')
-t.set_linewidth(0.1)
-t.set_alpha(0.5)
+	torus.set_alpha(1) # Transparency of figure
+	torus.set_edgecolor('w') # Edge color of the lines on the figure
+	torus.set_linewidth(1) # Line width of the edges
+	torus.set_facecolor('deepskyblue') # General color of the figure
 
-# Defintions for animations
-def init():
-    return t,
+# Defintions for animations	
+	def init():
+	    return torus,
 
-def animate(i):
-    # azimuth angle : 0 deg to 360 deg
-    # elev = i * n --> rotates object about the xy-plane with a magnitude of n
-    # azim = i * n --> rotates object around the z axis with a magnitude of n
-    # For top view elev = 90
-    # For side view elev = 0
+	def animate(i):
+# azimuth angle : 0 deg to 360 deg
+# elev = i * n --> rotates object about the xy-plane with a magnitude of n
+# azim = i * n --> rotates object around the z axis with a magnitude of n
+# For top view elev = 90
+# For side view elev = 0
 
-    ax.view_init(elev=50, azim= 4 * i)
-    return t,
+	    ax.view_init(elev=50, azim= 4 * i)
+	    return torus,
 
 
 # Animate
-ani = FuncAnimation(fig, animate, init_func=init,
-                    frames=36, interval=1, blit=False, repeat=True)
+	ani = FuncAnimation(fig, animate, init_func=init,
+    	               frames=36, interval=1, blit=False, repeat=True)
 
 # Saving to torus.mp4
 
-#Writer = writers['ffmpeg']
-#writer = Writer(fps=15, bitrate=1800)
-
-#ani.save('torus.mp4', writer=writer)
-
-plt.show()
+	# Writer = writers['ffmpeg']
+	# writer = Writer(fps=15, bitrate=1800)
+	
+	# if c > a:
+	#	name = 'Ring-Torus'
+	# elif c == a:
+	#	name = 'Horn-Torus'
+	#elif c < a:
+	#	name = 'Spindle-Torus'
+	
+	# ani.save('%s.mp4' % name, writer=writer)
+	
+	plt.show()
+	option = input('Run again? (0) Yes, (1) No\n>> ')
