f4c7472681c3e20d4c1fa5518cd230b8f8f1c637
Spencer Riley <42317614+PharaohCola13@users.noreply.github.com>
2018-09-04 16:19:45 -0600

    Update shell.py

--- Scripts/shell.py
+++ Scripts/shell.py
@@ -2,78 +2,88 @@
 
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import numpy as np
-from numpy import pi, linspace, cos, sin, tan, power
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
-from matplotlib.animation import FuncAnimation
-from matplotlib.animation import writers
 
-def x_(u,v):
-	x = power(1.2, v) * (sin(u) *sin(u) * sin(v))
-	return x
+from mpl_toolkits.mplot3d.art3d import *
+from matplotlib.animation import *
+from matplotlib import *
+from numpy import *
 
-def y_(u,v):
-	y = power(1.2, v) * (sin(u) *sin(u) * cos(v))
-	return y
+option = input('Run? (0) Yes, (1) No\n>> ')
 
-def z_(u,v):
-	z = power(1.2, v) * (sin(u) * cos(u))
-	return z
+while option == 0:
+# Definition of x
+	def x_(u,v):
+		x = power(1.2, v) * (sin(u)**2 * sin(v))
+		return x
 
-u = linspace(0, pi, 25)
-v = linspace(-pi/4, 5 * pi/2, 25)
+# Definition of y
+	def y_(u,v):
+		y = power(1.2, v) * (sin(u)**2 * cos(v))
+		return y
 
-u, v = np.meshgrid(u, v)
+# Definition of z
+	def z_(u,v):
+		z = power(1.2, v) * (sin(u) * cos(u))
+		return z
 
-x = x_(u,v)
-y = y_(u,v)
-z = z_(u,v)
+#Value of the angles
+	u = linspace(0, pi, 25)
+	v = linspace(-pi/4, 5 * pi/2, 25)
+
+	u, v = np.meshgrid(u, v)
+
+# Symbolic representation
+	x = x_(u,v)
+	y = y_(u,v)
+	z = z_(u,v)
 
 # Figure Properties
-fig = plt.figure(figsize=(8,8))
+	fig = plt.figure(figsize=(8,8))
 
-ax = p3.Axes3D(fig)
-ax.set_facecolor('black')
+	ax = p3.Axes3D(fig)
+	ax.set_facecolor('black') # Figure background turns black
 
-plt.axis('off')
-plt.axis('equal')
+# Axis Properties
+	plt.axis('off') # Turns off the axis grid
+	plt.axis('equal')
 
-#ax.set_xlim(-1,1)
-#ax.set_ylim(-1,1)
-#ax.set_zlim(-1,1)
+# Axis Limits
+	#ax.set_xlim(-1,1)
+	#ax.set_ylim(-1,1)
+	#ax.set_zlim(-1,1)
 
 # Surface Plot
-shell = ax.plot_surface(x, y, z)
+	shell = ax.plot_surface(x, y, z)
 
-shell.set_alpha(1)
-shell.set_edgecolor('w')
-shell.set_linewidth(1)
-shell.set_facecolor('deepskyblue')
+	shell.set_alpha(1) # Transparency of figure
+	shell.set_edgecolor('w') # Edge color of the lines on the figure
+	shell.set_linewidth(1) # Line width of the edges
+	shell.set_facecolor('deepskyblue') # General color of the figure
 
 # Definitions for animation
-def init():
-	return shell,
-
-def animate(i):
-    # azimuth angle : 0 deg to 360 deg
-    # elev = i * n --> rotates object about the xy-plane with a magnitude of n
-    # azim = i * n --> rotates object around the z axis with a magnitude of n
-    # For top view elev = 90
-    # For side view elev = 0
-
-   		ax.view_init(elev=i, azim=i*4)
+	def init():
 		return shell,
+
+	def animate(i):
+# azimuth angle : 0 deg to 360 deg
+# elev = i * n --> rotates object about the xy-plane with a magnitude of n
+# azim = i * n --> rotates object around the z axis with a magnitude of n
+# For top view elev = 90
+# For side view elev = 0
+
+	   		ax.view_init(elev=i, azim=i*4)
+			return shell,
 	
 # Animate
-#ani = FuncAnimation(fig, animate, init_func=init,
-#                   frames=100, interval=20, blit=False, repeat=True)
+	# ani = FuncAnimation(fig, animate, init_func=init,
+	#                   frames=100, interval=20, blit=False, repeat=True)
 
 # Saving to Shell.mp4
 
-#Writer = writers['ffmpeg']
-#writer = Writer(fps=15, bitrate=1800)
+	# Writer = writers['ffmpeg']
+	# writer = Writer(fps=15, bitrate=1800)
 
-#ani.save('Shell.mp4', writer=writer)
+	# ani.save('Shell.mp4', writer=writer)
 
-plt.show()
+	plt.show()
+	option = input('Run again? (0) Yes, (1) No\n>> ')
