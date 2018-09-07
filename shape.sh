4e488e2a9a52e2151b60cd6e400be0927822ca8b
Spencer Riley <spencerriley620@gmail.com>
2018-09-07 09:28:07 -0600

    Update shape.sh

--- shape.sh
+++ shape.sh
@@ -1,4 +1,22 @@
 #!/usr/bin
 
+echo -n "Object: "
+read shape
 
-if [
+echo "Rendering a ${shape}."
+
+if [ "${shape}" = "cube" ]; then
+
+   echo $(./Platonic Solids/cube.py)
+   
+elif [ "${shape}" = "sphere" ]; then
+   echo $(./sphere.py)
+   
+elif [ "${shape}" = "prism" ]; then
+   echo $(./prism.py)
+
+elif [ "${shape}" = "pyramid" ]; then 
+   echo $(./pyramid.py)
+   
+elif [ "${shape}" = "boy's surface" ]; then
+   echo $(./Current Models/Surfaces/boys_surface.py)
