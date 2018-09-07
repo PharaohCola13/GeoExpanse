271f8c4c9d71c06be0c2f9238b79a9aea8b9750e
PharaohCola13 <spencerriley620@gmail.com>
2018-09-06 22:18:47 -0600

    Update

--- gitForce.sh
+++ gitForce.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-read -p "Force: " force
-read -p "Repository: " Repo
+echo -n "Force: "
+read force
 
-$Repo = geometry
+echo -n  "Repository: "
+read Repo
 
-if [$force == 'push']
-then
-	read -p 'Exclusively pushing a README.md? ' readme
 
-	if (($readme == 'yes'))
-	then
-		git commit -m 'Update README.md'
-		echo "README.md has been commiting"
+if [ "${force}" = "push" ]; then
+	echo -n "Exclusively pushing a README.md? "
+    read readme
 
-	elif (($readme == 'no'))
-	then
-		read -p 'Message: ' message
-		git commit -m "$message"
-		echo "Files are commited"
+	if [[ ${readme} = "yes" ]]; then
+		echo $(git commit -m 'Update README.md')
+		echo "README.md has been committing."
 
-git push $Repo master
-echo "Files have been pushed tp $Repo"
+	elif [[ "${readme}" = "no" ]]; then
+		echo $(git commit -m "Update")
+		echo "Files are committed."
 
-if [$force == 'pull']
-then
-	git pull $Repo master
-	echo "Files have been pulled from $Repo"
-		
+    echo $(git push ${Repo} master)
+    echo "Force has been applied to ${Repo}."
 
+    fi
+
+elif [ "${force}" = "pull" ]; then
+	echo $(git pull ${Repo} master)
+	echo "Force has been applied to local repository."
+
+elif [ -z "${force}" ] && [ -z "${Repo}" ]; then
+    echo "Zero Newtons of force applied."
+
+fi
 
