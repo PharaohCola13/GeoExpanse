59c91c87f671081370c8b7901664dc82d9a25354
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 16:11:04 -0600

    Update steiner_surface.py

--- Current Models/Surfaces/steiner_surface.py
+++ Current Models/Surfaces/steiner_surface.py
@@ -2,15 +2,12 @@
 
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.pyplot as plt
-
-from mpl_toolkits.mplot3d.art3d import *
-from matplotlib.animation import *
 from matplotlib import *
 from numpy import *
+from mpl_toolkits.mplot3d.art3d import *
+from matplotlib.animation import *
 
-option = int(input('Run? (0) Yes, (1) No\n>> '))
-
-while option == 0:
+def shape(fig, alpha, color, edge_c, edge_w, rot_elev, rot_azim, grid, sides):
 # Definition of x
     def x_(u, v):
         x = sqrt(2) * cos(2 * u) * cos(v)**2 + cos(u) * sin(2 * v) / (2 - (b * sqrt(2) * sin(3 * u) * sin(2 * v)))
@@ -41,8 +38,6 @@ while option == 0:
     z = z_(u, v)
 
 # Figure Properties
-    fig = plt.figure(figsize=(8, 8))
-
     ax = p3.Axes3D(fig)
     ax.set_facecolor('black')  # Figure background turns black
 
@@ -58,38 +53,7 @@ while option == 0:
 # Surface Plot
     stein_surf = ax.plot_surface(x, y, z)
 
-    stein_surf.set_alpha(1)  # Transparency of figure
-    stein_surf.set_edgecolor('w')  # Edge color of the lines on the figure
-    stein_surf.set_linewidth(0.5)  # Line width of the edges
-    stein_surf.set_facecolor('deepskyblue')  # General color of the figure
-
-
-# Definitions for animation
-    def init():
-        return stein_surf,
-
-
-    def animate(i):
-# azimuth angle : 0 deg to 360 deg
-# elev = i * n --> rotates object about the xy-plane with a magnitude of n
-# azim = i * n --> rotates object around the z axis with a magnitude of n
-# For top view elev = 90
-# For side view elev = 0
-
-        ax.view_init(elev=29, azim=i * 4)
-        return stein_surf,
-
-
-# Animate
-    #ani = FuncAnimation(fig, animate, init_func=init,
-     #                   frames=100, interval=20, blit=False, repeat=True)
-
-# Saving to Steiner's-Surface.mp4
-
-    # Writer = writers['ffmpeg']
-    # writer = Writer(fps=15, bitrate=1800)
-
-    # ani.save('Steiner's-Surface.mp4', writer=writer)
-
-    plt.show()  # Shows Figure
-    option = int(input('Run again? (0) Yes, (1) No\n>> '))
+    stein_surf.set_alpha(alpha)  # Transparency of figure
+    stein_surf.set_edgecolor(edge.c)  # Edge color of the lines on the figure
+    stein_surf.set_linewidth(edge_w)  # Line width of the edges
+    stein_surf.set_facecolor(color)  # General color of the figure
