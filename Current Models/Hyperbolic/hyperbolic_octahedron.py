01744a4e433b2a9a7666a90b7f025f07ce5821ee
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 15:28:50 -0600

    Update hyperbolic_octahedron.py

--- Current Models/Hyperbolic/hyperbolic_octahedron.py
+++ Current Models/Hyperbolic/hyperbolic_octahedron.py
@@ -1,86 +1,71 @@
 # A Hyperbolic Octahedron, brought to you by PharaohCola13
-import sys
-sys.path.insert(0,'./parse.py')
-from  parse import *
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.pyplot as plt
+from matplotlib import *
+from numpy import *
+from mpl_toolkits.mplot3d.art3d import *
+from matplotlib.animation import *
 
 name = "Hyperbolic-Octahedron"
 
-if args.run:
-# Definition of x
-    def x_(u,v):
-        x = (cos(u) * cos(v))**3
-        return x
+def shape(fig, alpha, color, edge_c, edge_w, rot_elev, rot_azim, grid):
+	global name
+	# Definition of x
+	def x_(u,v):
+		x = (cos(u) * cos(v))**3
+		return x
 
-# Definition of y
-    def y_(u,v):
-        y = (sin(u) * cos(v))**3
-        return y
+	# Definition of y
+	def y_(u,v):
+		y = (sin(u) * cos(v))**3
+		return y
 
-# Definition of z
-    def z_(u,v):
-        z = sin(v)**3
-        return z
+	# Definition of z
+	def z_(u,v):
+		z = sin(v)**3
+		return z
 
-# Value of the angles
-    u = linspace(-pi/2, pi/2, 25)
-    v = linspace(-pi, pi, 25)
+	# Value of the angles
+	u = linspace(-pi/2, pi/2, 25)
+	v = linspace(-pi, pi, 25)
 
-    u, v = meshgrid(u, v)
+	u, v = meshgrid(u, v)
 
-# Symbolic representation
-    x = x_(u,v)
-    y = y_(u,v)
-    z = z_(u,v)
+	# Symbolic representation
+	x = x_(u,v)
+	y = y_(u,v)
+	z = z_(u,v)
 
-# Figure Properties
-    fig = plt.figure(figsize=(8,8))
+	# Figure Properties
+	ax = p3.Axes3D(fig)
+	ax.set_facecolor('black') # Figure background turns black
 
-    ax = p3.Axes3D(fig)
-    ax.set_facecolor('black') # Figure background turns black
+	# Axis Properties
+	plt.axis(grid)
+	plt.axis('equal')
 
-# Axis Properties
-    plt.axis('off') # Turns off the axis grid
-    plt.axis('equal')
+	# Axis Limits
+	ax.set_xlim(-1,1)
+	ax.set_ylim(-1,1)
+	ax.set_zlim(-1,1)
 
-# Axis Limits
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
+	# Surface Plot
+	hyper_octa = ax.plot_surface(x, y, z)
 
-# Surface Plot
-    hyper_octa = ax.plot_surface(x, y, z)
+	hyper_octa.set_alpha(alpha) # Transparency of figure
+	hyper_octa.set_edgecolor(edge_c) # Edge color of the lines on the figure
+	hyper_octa.set_linewidth(edge_w) # Line width of the edges
+	hyper_octa.set_facecolor(color) # General color of the figure
 
-    hyper_octa.set_alpha(args.alpha) # Transparency of figure
-    hyper_octa.set_edgecolor('w') # Edge color of the lines on the figure
-    hyper_octa.set_linewidth(1) # Line width of the edges
-    hyper_octa.set_facecolor(args.color) # General color of the figure
+	def animate(i):
+		global rot_elev, rot_azim
+	#     # azimuth angle : 0 deg to 360 deg
+	#     # elev = i * n --> rotates object about the xy-plane with a magnitude of n
+	#     # azim = i * n --> rotates object around the z axis with a magnitude of n
+	#     # For top view elev = 90
+	#     # For side view elev = 0
+	#
+		ax.view_init(elev=rot_elev * i, azim=rot_azim * i)
 
-    if args.rotate:
-# Definitions for animation
-        def init():
-            return hyper_octa,
-
-
-        def animate(i):
-# azimuth angle : 0 deg to 360 deg
-# elev = i * n --> rotates object about the xy-plane with a magnitude of n
-# azim = i * n --> rotates object around the z axis with a magnitude of n
-# For top view elev = 90
-# For side view elev = 0
-
-            ax.view_init(elev=-90, azim=i * 4)
-            return hyper_octa,
-
-
-# Animate
-        ani = FuncAnimation(fig, animate, init_func=init,
-                            frames=100, interval=20, blit=False, repeat=True)
-
-        if args.save:
-# Saving to Hyprbolic-Octahedron.mp4
-
-            Writer = writers['ffmpeg']
-            writer = Writer(fps=15, bitrate=1800)
-
-            ani.save('../Samples/%s.mp4' % name, writer=writer)
-plt.show()  # Shows Figure
+	ani = FuncAnimation(fig, animate,
+		   	frames=1000000, interval=1000, blit=False, repeat=True)
