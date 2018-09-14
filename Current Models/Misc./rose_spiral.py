1a064c688c7d45e599bccdc500b0dab7b776bee1
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 14:05:47 -0600

    Update rose_spiral.py

--- Current Models/Misc./rose_spiral.py
+++ Current Models/Misc./rose_spiral.py
@@ -1,16 +1,12 @@
 # A Rose Spiral, brought to you by PharaohCola13
 
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0,'./parse.py')
+from  parse import *
 
-from mpl_toolkits.mplot3d.art3d import *
-from matplotlib.animation import *
-from matplotlib import *
-from numpy import *
+name = "Rose-Spiral"
 
-option = int(input('Run? (0) Yes, (1) No\n>> '))
-
-while option == 0:
+if args.run:
     # Definition of x
     def x_(u, v):
         x = r * cos(u) * sin(v)
@@ -58,38 +54,37 @@ while option == 0:
     # Surface Plot
     rose = ax.plot_surface(x, y, z)
 
-    rose.set_alpha(1)  # Transparency of figure
+    rose.set_alpha(args.alpha)  # Transparency of figure
     rose.set_edgecolor('w')  # Edge color of the lines on the figure
     rose.set_linewidth(1)  # Line width of the edges
-    rose.set_facecolor('deepskyblue')  # General color of the figure
+    rose.set_facecolor(args.color)  # General color of the figure
 
-
+    if args.rotate:
     # Definitions for animation
-    def init():
-        return rose,
+        def init():
+            return rose,
 
 
-    def animate(i):
-        # azimuth angle : 0 deg to 360 deg
-        # elev = i * n --> rotates object about the xy-plane with a magnitude of n
-        # azim = i * n --> rotates object around the z axis with a magnitude of n
-        # For top view elev = 90
-        # For side view elev = 0
+        def animate(i):
+            # azimuth angle : 0 deg to 360 deg
+            # elev = i * n --> rotates object about the xy-plane with a magnitude of n
+            # azim = i * n --> rotates object around the z axis with a magnitude of n
+            # For top view elev = 90
+            # For side view elev = 0
 
-        ax.view_init(elev=29, azim=i * 4)
-        return rose,
+            ax.view_init(elev=29, azim=i * 4)
+            return rose,
 
-
+    
     # Animate
-    #ani = FuncAnimation(fig, animate, init_func=init,
-     #                   frames=100, interval=20, blit=False, repeat=True)
-
+        ani = FuncAnimation(fig, animate, init_func=init,
+                        frames=100, interval=20, blit=False, repeat=True)
+        if args.save:
     # Saving to Rose-Spiral.mp4
 
-    # Writer = writers['ffmpeg']
-    # writer = Writer(fps=15, bitrate=1800)
+            Writer = writers['ffmpeg']
+            writer = Writer(fps=15, bitrate=1800)
 
-    # ani.save('Rose-Spiral.mp4', writer=writer)
+            ani.save('../Samples/%s.mp4' % name, writer=writer)
 
     plt.show()  # Shows Figure
-    option = int(input('Run again? (0) Yes, (1) No\n>> '))
