83a4d9ac57da952fbabd1c536784bc2a7b42e87b
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 16:09:44 -0600

    Update prism.py

--- Current Models/prism.py
+++ Current Models/prism.py
@@ -63,25 +63,3 @@ def shape(fig, alpha, color, edge_c, edge_w, rot_elev, rot_azim, grid, sides):
 	prism.set_edgecolor(edge_c) # Edge color of the lines on the figure
 	prism.set_linewidth(edge_w) # Line width of the edges
 	prism.set_facecolor(color) # General color of the figure
-
-# Definitions for animation
-	def animate(i):
-# azimuth angle : 0 deg to 360 deg
-# elev = i * n --> rotates object about the xy-plane with a magnitude of n
-# azim = i * n --> rotates object around the z axis with a magnitude of n
-# For top view elev = 90
-# For side view elev = 0
-
-    		ax.view_init(elev=i, azim=i*4)
-    		return prism,
-
-# Animate
-	ani = FuncAnimation(fig, animate, init_func=init,
-                    frames=100, interval=20, blit=False, repeat=True)
-# Saving to Prism.mp4
-
-	#Writer = writers['ffmpeg']
-	#writer = Writer(fps=15, bitrate=1800)
-
-	#name = '%s-Prism' % int(s)
-	#ani.save('%s.mp4' % name, writer=writer)
