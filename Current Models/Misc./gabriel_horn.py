ab2ec8204e7780380c550268dc38faa5de3619e3
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 15:41:26 -0600

    Update gabriel_horn.py

--- Current Models/Misc./gabriel_horn.py
+++ Current Models/Misc./gabriel_horn.py
@@ -1,12 +1,15 @@
 # A Gabriel's Horn, brought to you by PharaohCola13
 
-import sys
-sys.path.insert(0,'../')
-from parse import *
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.pyplot as plt
+from matplotlib import *
+from numpy import *
+from mpl_toolkits.mplot3d.art3d import *
+from matplotlib.animation import *
 
 name = "Gabriel's-Horn"
 
-if args.run:
+def shape(fig, alpha, color, edge_c, edge_w, rot_elev, rot_azim, grid, sides):
     # Definition of x
     def x_(u, v):
         x = u
@@ -23,9 +26,9 @@ if args.run:
         z = (a * sin(v)) /u
         return z
 
-    a = args.radius # changes radius of the entire thing
+    a = 1 # changes radius of the entire thing
 
-    h = args.height
+    h = 1
 
     # Value of the angles
     u = linspace(1, h, 25)
@@ -39,13 +42,11 @@ if args.run:
     z = z_(u, v)
 
     # Figure Properties
-    fig = plt.figure(figsize=(8, 8))
-
     ax = p3.Axes3D(fig)
     ax.set_facecolor('black')  # Figure background turns black
 
     # Axis Properties
-    plt.axis('off')  # Turns off the axis grid
+    plt.axis(grid)  # Turns off the axis grid
     plt.axis('equal')
 
     # Axis Limits
@@ -56,16 +57,12 @@ if args.run:
     # Surface Plot
     horn = ax.plot_surface(x, y, z)
 
-    horn.set_alpha(args.alpha)  # Transparency of figure
-    horn.set_edgecolor('w')  # Edge color of the lines on the figure
-    horn.set_linewidth(1)  # Line width of the edges
-    horn.set_facecolor(args.color)  # General color of the figure
+    horn.set_alpha(alpha)  # Transparency of figure
+    horn.set_edgecolor(edge_c)  # Edge color of the lines on the figure
+    horn.set_linewidth(edge_w)  # Line width of the edges
+    horn.set_facecolor(color)  # General color of the figure
 
-    if args.rotate:
 # Definitions for animation
-        def init():
-            return horn,
-
 
         def animate(i):
 # azimuth angle : 0 deg to 360 deg
@@ -79,14 +76,5 @@ if args.run:
 
 
 # Animate
-            ani = FuncAnimation(fig, animate, init_func=init,
+            ani = FuncAnimation(fig, animate,
                         frames=100, interval=20, blit=False, repeat=True)
-        if args.save:
-# Saving to Gabriel's-Horn.mp4
-
-            Writer = writers['ffmpeg']
-            writer = Writer(fps=15, bitrate=1800)
-
-            ani.save('%s' % name, writer=writer)
-
-plt.show()  # Shows Figure
