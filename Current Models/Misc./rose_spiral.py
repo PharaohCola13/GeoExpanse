523680b0a824411d5de3ca1bc7610faea9782530
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 15:41:57 -0600

    Update rose_spiral.py

--- Current Models/Misc./rose_spiral.py
+++ Current Models/Misc./rose_spiral.py
@@ -1,12 +1,13 @@
 # A Rose Spiral, brought to you by PharaohCola13
 
-import sys
-sys.path.insert(0,'./parse.py')
-from  parse import *
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.pyplot as plt
+from matplotlib import *
+from numpy import *
+from mpl_toolkits.mplot3d.art3d import *
+from matplotlib.animation import *
 
-name = "Rose-Spiral"
-
-if args.run:
+def shape(fig, alpha, color, edge_c, edge_w, rot_elev, rot_azim, grid, sides):
     # Definition of x
     def x_(u, v):
         x = r * cos(u) * sin(v)
@@ -37,13 +38,11 @@ if args.run:
     z = z_(u, v)
 
     # Figure Properties
-    fig = plt.figure(figsize=(8, 8))
-
     ax = p3.Axes3D(fig)
     ax.set_facecolor('black')  # Figure background turns black
 
     # Axis Properties
-    plt.axis('off')  # Turns off the axis grid
+    plt.axis(grid)  # Turns off the axis grid
     plt.axis('equal')
 
     # Axis Limits
@@ -54,37 +53,31 @@ if args.run:
     # Surface Plot
     rose = ax.plot_surface(x, y, z)
 
-    rose.set_alpha(args.alpha)  # Transparency of figure
-    rose.set_edgecolor('w')  # Edge color of the lines on the figure
-    rose.set_linewidth(1)  # Line width of the edges
-    rose.set_facecolor(args.color)  # General color of the figure
+    rose.set_alpha(alpha)  # Transparency of figure
+    rose.set_edgecolor(edge_c)  # Edge color of the lines on the figure
+    rose.set_linewidth(edge_w)  # Line width of the edges
+    rose.set_facecolor(color)  # General color of the figure
 
-    if args.rotate:
+
     # Definitions for animation
-        def init():
-            return rose,
+    def animate(i):
+        # azimuth angle : 0 deg to 360 deg
+        # elev = i * n --> rotates object about the xy-plane with a magnitude of n
+        # azim = i * n --> rotates object around the z axis with a magnitude of n
+        # For top view elev = 90
+        # For side view elev = 0
+
+        ax.view_init(elev=29, azim=i * 4)
+        return rose,
 
 
-        def animate(i):
-            # azimuth angle : 0 deg to 360 deg
-            # elev = i * n --> rotates object about the xy-plane with a magnitude of n
-            # azim = i * n --> rotates object around the z axis with a magnitude of n
-            # For top view elev = 90
-            # For side view elev = 0
-
-            ax.view_init(elev=29, azim=i * 4)
-            return rose,
-
-    
     # Animate
-        ani = FuncAnimation(fig, animate, init_func=init,
-                        frames=100, interval=20, blit=False, repeat=True)
-        if args.save:
+    ani = FuncAnimation(fig, animate,
+                       frames=100, interval=20, blit=False, repeat=True)
+
     # Saving to Rose-Spiral.mp4
 
-            Writer = writers['ffmpeg']
-            writer = Writer(fps=15, bitrate=1800)
+    # Writer = writers['ffmpeg']
+    # writer = Writer(fps=15, bitrate=1800)
 
-            ani.save('../Samples/%s.mp4' % name, writer=writer)
-
-    plt.show()  # Shows Figure
+    # ani.save('Rose-Spiral.mp4', writer=writer)
