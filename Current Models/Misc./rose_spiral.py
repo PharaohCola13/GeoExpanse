69da51ec855b5ad469630c506cdf6fd20c6993b6
Spencer Riley <spencerriley620@gmail.com>
2018-09-14 16:07:00 -0600

    Update rose_spiral.py

--- Current Models/Misc./rose_spiral.py
+++ Current Models/Misc./rose_spiral.py
@@ -57,27 +57,3 @@ def shape(fig, alpha, color, edge_c, edge_w, rot_elev, rot_azim, grid, sides):
     rose.set_edgecolor(edge_c)  # Edge color of the lines on the figure
     rose.set_linewidth(edge_w)  # Line width of the edges
     rose.set_facecolor(color)  # General color of the figure
-
-
-    # Definitions for animation
-    def animate(i):
-        # azimuth angle : 0 deg to 360 deg
-        # elev = i * n --> rotates object about the xy-plane with a magnitude of n
-        # azim = i * n --> rotates object around the z axis with a magnitude of n
-        # For top view elev = 90
-        # For side view elev = 0
-
-        ax.view_init(elev=29, azim=i * 4)
-        return rose,
-
-
-    # Animate
-    ani = FuncAnimation(fig, animate,
-                       frames=100, interval=20, blit=False, repeat=True)
-
-    # Saving to Rose-Spiral.mp4
-
-    # Writer = writers['ffmpeg']
-    # writer = Writer(fps=15, bitrate=1800)
-
-    # ani.save('Rose-Spiral.mp4', writer=writer)
