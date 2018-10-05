e1b1740510b07b79b3223a9112be27de7864d4bf
Spencer Riley <spencerriley620@gmail.com>
2018-10-05 16:15:14 -0600

    Update great_icosahedron.py

--- In Development/great_icosahedron.py
+++ In Development/great_icosahedron.py
@@ -7,83 +7,82 @@ from numpy import *
 from mpl_toolkits.mplot3d.art3d import *
 from matplotlib.animation import *
 
-name = "Cube"
+name = "Great Icosahedron"
 
+def shape(fig, alpha, color, edge_c, edge_w, grid):
 
-points = array([[0,0,0],
-				[0, 0, -0.5 * sqrt(0.5 * (5 - sqrt(5)))],
-				[0, 0, 0.5 * sqrt(0.5 * (5 - sqrt(5)))],
-				[sqrt(((1./8.)+(1/(8 *sqrt(5))))), -1/(1 +sqrt(5)), -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				[sqrt(((1./8.) + (1/(8 * sqrt(5))))), 1/(1 + sqrt(5)), -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				[-sqrt(((1./8.) + (1/(8 * sqrt(5))))), -1/(1 + sqrt(5)), 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				[-sqrt(((1./8.) + (1/(8 * sqrt(5))))), 1/(1 + sqrt(5)), 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				[sqrt(0.1 * (5 - sqrt(5))),0, 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				[-sqrt(0.1 * (5 - sqrt(5))), 0, -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				[-0.5 *sqrt(1 - (2/sqrt(5))), -0.5, -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-                [-0.5 *sqrt(1 - (2/sqrt(5))), 0.5, -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-                [0.5 *sqrt(1 - (2/sqrt(5))), -0.5, 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-                [0.5 *sqrt(1 - (2/sqrt(5))), 0.5, 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
-				])
+	points = array([[0,0,0],
+					[0, 0, -0.5 * sqrt(0.5 * (5 - sqrt(5)))],
+					[0, 0, 0.5 * sqrt(0.5 * (5 - sqrt(5)))],
+					[sqrt(((1./8.)+(1/(8 *sqrt(5))))), -1/(1 +sqrt(5)), -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					[sqrt(((1./8.) + (1/(8 * sqrt(5))))), 1/(1 + sqrt(5)), -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					[-sqrt(((1./8.) + (1/(8 * sqrt(5))))), -1/(1 + sqrt(5)), 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					[-sqrt(((1./8.) + (1/(8 * sqrt(5))))), 1/(1 + sqrt(5)), 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					[sqrt(0.1 * (5 - sqrt(5))),0, 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					[-sqrt(0.1 * (5 - sqrt(5))), 0, -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					[-0.5 *sqrt(1 - (2/sqrt(5))), -0.5, -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+		            [-0.5 *sqrt(1 - (2/sqrt(5))), 0.5, -0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+		            [0.5 *sqrt(1 - (2/sqrt(5))), -0.5, 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+		            [0.5 *sqrt(1 - (2/sqrt(5))), 0.5, 0.5 * sqrt(0.1 * (5 - sqrt(5)))],
+					])
 
-# Scaling Matricies
-P = [[1, 0, 0],
-	 [0, 1, 0],
-	 [0, 0, 1]]
+	# Scaling Matricies
+	P = [[1, 0, 0],
+		 [0, 1, 0],
+		 [0, 0, 1]]
 
-Z = zeros((13, 3))
+	Z = zeros((13, 3))
 
-for i in range(13):
-	Z[i, :] = dot(points[i, :], P)
+	for i in range(13):
+		Z[i, :] = dot(points[i, :], P)
 
-# Figure Properties
-fig = plt.figure(figsize=(8, 8))
-ax = p3.Axes3D(fig)
-ax.set_facecolor('black')
+	# Figure Properties
+	fig = plt.figure(figsize=(8, 8))
+	ax = p3.Axes3D(fig)
+	ax.set_facecolor('black')
 
-plt.axis("off")
-plt.axis('equal')
+	plt.axis(grid)
+	plt.axis('equal')
 
-ax.set_xlim(-4, 4)
-ax.set_ylim(-4, 4)
-ax.set_zlim(-4, 4)
+	ax.set_xlim(-4, 4)
+	ax.set_ylim(-4, 4)
+	ax.set_zlim(-4, 4)
 
-# Interval
-r = [-1, 1]
+	# Interval
+	r = [-1, 1]
 
-X, Y = np.meshgrid(r, r)
+	X, Y = np.meshgrid(r, r)
 
-# Side Configuration for Cube
-verts_cube = [[Z[1], Z[7], Z[5]],
-			  [Z[6], Z[7], Z[1]],
-			  [Z[1], Z[5], Z[12]],
-			  [Z[11], Z[6], Z[1]],
-			  [Z[12], Z[11], Z[1]],
-			  [Z[2], Z[8], Z[3]],
-			  [Z[4], Z[8], Z[2]],
-			  [Z[2], Z[3], Z[10]],
-			  [Z[9], Z[4], Z[2]],
-			  [Z[10], Z[9], Z[2]],
-			  [Z[12], Z[3], Z[8]],
-			  [Z[8], Z[4], Z[11]],
-			  [Z[8], Z[11], Z[12]],
-			  [Z[10], Z[5], Z[7]],
-			  [Z[7], Z[6], Z[9]],
-			  [Z[7], Z[9], Z[10]],
-			  [Z[3], Z[5], Z[10]],
-			  [Z[12], Z[5], Z[3]],
-			  [Z[9], Z[6], Z[4]],
-			  [Z[4], Z[6], Z[11]]
-			  ]
-# Cube Properties
+	# Side Configuration for Cube
+	verts_cube = [[Z[1], Z[7], Z[5]],
+				  [Z[6], Z[7], Z[1]],
+				  [Z[1], Z[5], Z[12]],
+				  [Z[11], Z[6], Z[1]],
+				  [Z[12], Z[11], Z[1]],
+				  [Z[2], Z[8], Z[3]],
+				  [Z[4], Z[8], Z[2]],
+				  [Z[2], Z[3], Z[10]],
+				  [Z[9], Z[4], Z[2]],
+				  [Z[10], Z[9], Z[2]],
+				  [Z[12], Z[3], Z[8]],
+				  [Z[8], Z[4], Z[11]],
+				  [Z[8], Z[11], Z[12]],
+				  [Z[10], Z[5], Z[7]],
+				  [Z[7], Z[6], Z[9]],
+				  [Z[7], Z[9], Z[10]],
+				  [Z[3], Z[5], Z[10]],
+				  [Z[12], Z[5], Z[3]],
+				  [Z[9], Z[6], Z[4]],
+				  [Z[4], Z[6], Z[11]]
+				  ]
+	# Cube Properties
 
-cube = Poly3DCollection(verts_cube)
+	cube = Poly3DCollection(verts_cube)
 
-cube.set_edgecolor("blue")
-cube.set_linewidth(1)
-cube.set_alpha(0.5)
-cube.set_facecolor("cyan")
+	cube.set_edgecolor(edge_c)
+	cube.set_linewidth(edge_w)
+	cube.set_alpha(alpha)
+	cube.set_facecolor(color)
 
-# Plot Surfaces
-ax.add_collection3d(cube)
-
-plt.show()
+	# Plot Surfaces
+	ax.add_collection3d(cube)
