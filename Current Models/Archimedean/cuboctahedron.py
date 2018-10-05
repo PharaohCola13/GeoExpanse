f7e6e212db7708b5f30de8edfc0944af7327d05b
Spencer Riley <spencerriley620@gmail.com>
2018-10-05 16:06:02 -0600

    Update cuboctahedron.py

--- Current Models/Archimedean/cuboctahedron.py
+++ Current Models/Archimedean/cuboctahedron.py
@@ -9,8 +9,7 @@ from matplotlib.animation import *
 
 name = "Cuboctahedron"
 
-def shape(fig, alpha, color, edge_c, edge_w, grid, sides,
-				   edges, multi_pi, radius):	
+def shape(fig, alpha, color, edge_c, edge_w, grid, color2, color3):	
 
 
 	points = array([[0,0,0],
@@ -39,7 +38,6 @@ def shape(fig, alpha, color, edge_c, edge_w, grid, sides,
 		Z[i, :] = dot(points[i, :], P)
 
 	# Figure Properties
-	#fig = plt.figure(figsize=(5, 5))
 	ax = p3.Axes3D(fig)
 	ax.set_facecolor('black')
 
@@ -50,26 +48,22 @@ def shape(fig, alpha, color, edge_c, edge_w, grid, sides,
 	ax.set_ylim(-1, 1)
 	ax.set_zlim(-1, 1)
 
-	# Interval
-	r = [-1, 1]
-
-	X, Y = np.meshgrid(r, r)
-
-
-	verts_cuboc = [[Z[4], Z[10], Z[8], Z[2]],
-		          [Z[3], Z[9], Z[11], Z[5]],
-		          [Z[9], Z[6], Z[8], Z[12]],
-		          [Z[3], Z[1], Z[2], Z[6]],
-		          [Z[5], Z[7], Z[4], Z[1]],
+	verts_cuboc = [[Z[4], Z[10], Z[8],  Z[2]],
+		          [Z[3],  Z[9],  Z[11], Z[5]],
+		          [Z[9],  Z[6],  Z[8],  Z[12]],
+		          [Z[3],  Z[1],  Z[2],  Z[6]],
+		          [Z[5],  Z[7],  Z[4],  Z[1]],
 		          [Z[11], Z[12], Z[10], Z[7]],
+				]
+	cuboc_three = [
 		          [Z[12], Z[11], Z[9]],
-		          [Z[3], Z[5], Z[1]],
-		          [Z[6], Z[9], Z[3]],
-		          [Z[5], Z[11], Z[7]],
-		          [Z[8], Z[10], Z[12]],
-		          [Z[1], Z[4], Z[2]],
-		          [Z[2], Z[8], Z[6]],
-		          [Z[7], Z[10], Z[4]]
+		          [Z[3],  Z[5],  Z[1]],
+		          [Z[6],  Z[9],  Z[3]],
+		          [Z[5],  Z[11], Z[7]],
+		          [Z[8],  Z[10], Z[12]],
+		          [Z[1],  Z[4],  Z[2]],
+		          [Z[2],  Z[8],  Z[6]],
+		          [Z[7],  Z[10], Z[4]]
 		          ]
 
 	cuboc = Poly3DCollection(verts_cuboc)
@@ -79,5 +73,13 @@ def shape(fig, alpha, color, edge_c, edge_w, grid, sides,
 	cuboc.set_alpha(alpha)
 	cuboc.set_facecolor(color)
 
+	cuboc1 = Poly3DCollection(cuboc_three)
+
+	cuboc1.set_edgecolor(edge_c)
+	cuboc1.set_linewidth(edge_w)
+	cuboc1.set_alpha(alpha)
+	cuboc1.set_facecolor(color2)
+
 	# Plot Surfaces
 	ax.add_collection3d(cuboc)
+	ax.add_collection3d(cuboc1)
