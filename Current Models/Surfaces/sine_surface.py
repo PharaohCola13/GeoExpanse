3e0613ca254ded0aff5130b7f2ca228fa60f1dca
Spencer Riley <spencerriley620@gmail.com>
2018-10-05 16:12:00 -0600

    Update sine_surface.py

--- Current Models/Surfaces/sine_surface.py
+++ Current Models/Surfaces/sine_surface.py
@@ -7,54 +7,55 @@ from numpy import *
 from mpl_toolkits.mplot3d.art3d import *
 from matplotlib.animation import *
 
-
 name = "Sine Surface"
 
-def shape(fig, alpha, color, edge_c, edge_w, grid, sides, edges, multi_pi):
-    # Definition of x
-    def x_(u,v):
-            x = a * sin(u)
-            return x
+def shape(fig, alpha, color, edge_c, edge_w, grid, sides, edges, multi_pi, radius):
 
-    # Definition of y
-    def y_(u,v):
-            y = a * sin(v)
-            return y
+	# Definition of x
+	def x_(u,v):
+		    x = a * sin(u)
+		    return x
 
-    # Definition of z
-    def z_(u,v):
-            z  = a * sin(u + v)
-            return z
+	# Definition of y
+	def y_(u,v):
+		    y = a * sin(v)
+		    return y
 
-    a = 1 #
-    # Values of the angles
-    u = linspace(0, 2 * pi, 50)
-    v = linspace(0, 2 * pi, 50)
+	# Definition of z
+	def z_(u,v):
+		    z  = a * sin(u + v)
+		    return z
 
-    u, v = meshgrid(u, v)
+	a = radius #
+	# Values of the angles
+	s = sides
+	u = linspace(0, 2 * pi, edges)
+	v = linspace(0, 2 * pi, s + 1)
 
-    # Symbolic Representation
-    x = x_(u,v)
-    y = y_(u,v)
-    z = z_(u,v)
+	u, v = meshgrid(u, v)
 
-    # Figure Properties
-    ax = p3.Axes3D(fig)
-    ax.set_facecolor('black') # Figure background turns black
+	# Symbolic Representation
+	x = x_(u,v)
+	y = y_(u,v)
+	z = z_(u,v)
 
-    # Axis Properties
-    plt.axis(grid) # Turns off the axis grid
-    plt.axis('equal')
+	# Figure Properties
+	ax = p3.Axes3D(fig)
+	ax.set_facecolor('black') # Figure background turns black
 
-    # Axis Limits
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
+	# Axis Properties
+	plt.axis(grid) # Turns off the axis grid
+	plt.axis('equal')
 
-    # Surface Plot
-    sine = ax.plot_surface(x, y, z)
+	# Axis Limits
+	#ax.set_xlim(-1,1)
+	#ax.set_ylim(-1,1)
+	#ax.set_zlim(-1,1)
 
-    sine.set_alpha(alpha) # Transparency of figure
-    sine.set_edgecolor(edge_c) # Edge color of the lines on the figure
-    sine.set_linewidth(edge_w) # Line width of the edges
-    sine.set_facecolor(color) # General color of the figure
+	# Surface Plot
+	sine = ax.plot_surface(x, y, z)
+
+	sine.set_alpha(alpha) # Transparency of figure
+	sine.set_edgecolor(edge_c) # Edge color of the lines on the figure
+	sine.set_linewidth(edge_w) # Line width of the edges
+	sine.set_facecolor(color) # General color of the figure
