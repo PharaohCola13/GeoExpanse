8abf529443f5c3ebedb3180772036742ae1fa398
Spencer Riley <spencerriley620@gmail.com>
2018-10-05 16:18:29 -0600

    Update testing.py

--- In Development/testing.py
+++ In Development/testing.py
@@ -11,7 +11,7 @@ from numpy import *
 name = "Testing"
 
 def shape(fig, alpha, color, edge_c, edge_w, grid, sides,
-				   edges, multi_pi, radius):
+					   edges, multi_pi, radiusm, radiusa, color2, color3, height):
 	def x_(u,v):
 		x = cos(u) * sin(v)
 		return x
