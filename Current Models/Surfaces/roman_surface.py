3dd0b8c7426a43e2011e6d18bb93172b12e752e3
Spencer Riley <spencerriley620@gmail.com>
2018-10-05 16:11:46 -0600

    Update roman_surface.py

--- Current Models/Surfaces/roman_surface.py
+++ Current Models/Surfaces/roman_surface.py
@@ -9,54 +9,47 @@ from matplotlib.animation import *
 
 name = "Roman Surface"
 
-def shape(fig, alpha, color, edge_c, edge_w, grid, sides,
-				   edges, multi_pi, radius):
+def shape(fig, alpha, color, edge_c, edge_w, grid, sides, edges, multi_pi):
     # Definition of x
-    def x_(u,v):
-            x = ((sqrt(2) * cos(2*u)*cos(v)**2) + (cos(u) * sin(2*v)))/(2 - a*sqrt(2) * sin(3*u)*sin(2*v))
-            return x
+	def x_(u,v):
+		    x = ((sqrt(2) * cos(2*u)*cos(v)**2) + (cos(u) * sin(2*v)))/(2 - a*sqrt(2) * sin(3*u)*sin(2*v))
+		    return x
 
-    # Definition of y
-    def y_(u,v):
-            y = ((sqrt(2) * sin(2*u)*cos(v)**2) - (sin(u) * sin(2*v)))/(2 - a*sqrt(2) * sin(3*u)*sin(2*v))
-            return y
+	# Definition of y
+	def y_(u,v):
+		    y = ((sqrt(2) * sin(2*u)*cos(v)**2) - (sin(u) * sin(2*v)))/(2 - a*sqrt(2) * sin(3*u)*sin(2*v))
+		    return y
 
-    # Definition of z
-    def z_(u,v):
-            z  = (3 * cos(v)**2)/(2 - a*sqrt(2) * sin(3*u)*sin(2*v))
-            return z
+	# Definition of z
+	def z_(u,v):
+		    z  = (3 * cos(v)**2)/(2 - a*sqrt(2) * sin(3*u)*sin(2*v))
+		    return z
 
-    a = 0 # a = 0, Roman Surface. a = 1, Boys Surface
-    # Values of the angles
-    u = linspace(0, 2 * pi, 50)
-    v = linspace(0, pi/2, 50)
+	a = 0 # a = 0, Roman Surface. a = 1, Boys Surface
+	# Values of the angles
+	s = sides
+	u = linspace(0, 2 * pi, s + 1)
+	v = linspace(0, pi/2, edges)
 
-    u, v = meshgrid(u, v)
+	u, v = meshgrid(u, v)
 
-    # Symbolic Representation
-    x = x_(u,v)
-    y = y_(u,v)
-    z = z_(u,v)
+	# Symbolic Representation
+	x = x_(u,v)
+	y = y_(u,v)
+	z = z_(u,v)
 
-    # Figure Properties
-#   fig = plt.figure(figsize=(8,8))
+	# Figure Properties
+	ax = p3.Axes3D(fig)
+	ax.set_facecolor('black') # Figure background turns black
 
-    ax = p3.Axes3D(fig)
-    ax.set_facecolor('black') # Figure background turns black
+	# Axis Properties
+	plt.axis(grid) # Turns off the axis grid
+	plt.axis('equal')
 
-    # Axis Properties
-    plt.axis(grid) # Turns off the axis grid
-    plt.axis('equal')
+	# Surface Plot
+	roman = ax.plot_surface(x, y, z)
 
-    #Axis Limits
-    # plt.xlim(-1 * zoom, zoom)
-    # plt.ylim(-1 * zoom, zoom)
-    # ax.set_zlim(-1 * zoom,zoom)
-
-    # Surface Plot
-    roman = ax.plot_surface(x, y, z)
-
-    roman.set_alpha(alpha) # Transparency of figure
-    roman.set_edgecolor(edge_c) # Edge color of the lines on the figure
-    roman.set_linewidth(edge_w) # Line width of the edges
-    roman.set_facecolor(color) # General color of the figure
+	roman.set_alpha(alpha) # Transparency of figure
+	roman.set_edgecolor(edge_c) # Edge color of the lines on the figure
+	roman.set_linewidth(edge_w) # Line width of the edges
+	roman.set_facecolor(color) # General color of the figure
