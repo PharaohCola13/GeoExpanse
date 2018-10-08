12c95c6963a469fdb62d8b68b5f5925210ccdb70
Spencer Riley <spencerriley620@gmail.com>
2018-10-08 17:21:33 -0600

    Update snub_cube.py

--- Current Models/Archimedean/snub_cube.py
+++ Current Models/Archimedean/snub_cube.py
@@ -63,61 +63,67 @@ def shape(fig, alpha, color, edge_c, edge_w, grid, color2):
 	ax.set_zlim(-1, 1)
 
 	# Side Configuration for scube
+
 	verts_scube = [ [Z[2],Z[12],Z[0],Z[14]],
-		            [Z[3],Z[13],Z[1],Z[15]],
-		            [Z[4],Z[16],Z[5],Z[17]],
-		            [Z[7],Z[19],Z[6],Z[18]],
-		            [Z[8],Z[21],Z[11],Z[22]],
-		            [Z[9],Z[20],Z[10],Z[23]],
+			        [Z[3],Z[13],Z[1],Z[15]],
+			        [Z[4],Z[16],Z[5],Z[17]],
+			        [Z[7],Z[19],Z[6],Z[18]],
+			        [Z[8],Z[21],Z[11],Z[22]],
+			        [Z[9],Z[20],Z[10],Z[23]],
 					]
 	verts_tri =   [
-		            [Z[0],  Z[8],  Z[14]],
-		            [Z[1],  Z[9],  Z[15]],
-		            [Z[2],  Z[10], Z[12]],
-		            [Z[3],  Z[11], Z[13]],
-		            [Z[4],  Z[0],  Z[16]],
-		            [Z[5],  Z[1],  Z[17]],
-		            [Z[6],  Z[2],  Z[18]],
-		            [Z[7],  Z[3],  Z[19]],
-		            [Z[8],  Z[4],  Z[21]],
-		            [Z[9],  Z[5],  Z[20]],
-		            [Z[10], Z[6],  Z[23]],
-		            [Z[11], Z[7],  Z[22]],
-		            [Z[12], Z[16], Z[0]],
-		            [Z[13], Z[17], Z[1]],
-		            [Z[14], Z[18], Z[2]],
-		            [Z[15], Z[19], Z[3]],
-		            [Z[16], Z[20], Z[5]],
-		            [Z[17], Z[21], Z[4]],
-		            [Z[18], Z[22], Z[7]],
-		            [Z[19], Z[23], Z[6]],
-		            [Z[20], Z[12], Z[10]],
-		            [Z[21], Z[13], Z[11]],
-		            [Z[22], Z[14], Z[8]],
-		            [Z[23], Z[15], Z[9]],
-		            [Z[8],  Z[0],  Z[4]],
-		            [Z[9],  Z[1],  Z[5]],
-		            [Z[10], Z[2],  Z[6]],
-		            [Z[11], Z[3],  Z[7]],
-		            [Z[12], Z[20], Z[16]],
-		            [Z[13], Z[21], Z[17]],
-		            [Z[14], Z[22], Z[18]],
-		            [Z[15], Z[23], Z[19]],
-		           ]
+			        [Z[0],  Z[8],  Z[14]],
+			        [Z[1],  Z[9],  Z[15]],
+			        [Z[2],  Z[10], Z[12]],
+			        [Z[3],  Z[11], Z[13]],
+			        [Z[4],  Z[0],  Z[16]],
+			        [Z[5],  Z[1],  Z[17]],
+			        [Z[6],  Z[2],  Z[18]],
+			        [Z[7],  Z[3],  Z[19]],
+			        [Z[8],  Z[4],  Z[21]],
+			        [Z[9],  Z[5],  Z[20]],
+			        [Z[10], Z[6],  Z[23]],
+			        [Z[11], Z[7],  Z[22]],
+			        [Z[12], Z[16], Z[0]],
+			        [Z[13], Z[17], Z[1]],
+			        [Z[14], Z[18], Z[2]],
+			        [Z[15], Z[19], Z[3]],
+			        [Z[16], Z[20], Z[5]],
+			        [Z[17], Z[21], Z[4]],
+			        [Z[18], Z[22], Z[7]],
+			        [Z[19], Z[23], Z[6]],
+			        [Z[20], Z[12], Z[10]],
+			        [Z[21], Z[13], Z[11]],
+			        [Z[22], Z[14], Z[8]],
+			        [Z[23], Z[15], Z[9]],
+			        [Z[8],  Z[0],  Z[4]],
+			        [Z[9],  Z[1],  Z[5]],
+			        [Z[10], Z[2],  Z[6]],
+			        [Z[11], Z[3],  Z[7]],
+			        [Z[12], Z[20], Z[16]],
+			        [Z[13], Z[21], Z[17]],
+			        [Z[14], Z[22], Z[18]],
+			        [Z[15], Z[23], Z[19]],
+			       ]
 
-	scube = Poly3DCollection(verts_scube)
+	verts =verts_scube + verts_tri
+	scube = Poly3DCollection(verts)
 
 	scube.set_edgecolor(edge_c)
 	scube.set_linewidth(edge_w)
 	scube.set_alpha(alpha)
 	scube.set_facecolor(color)
 
-	scube1 = Poly3DCollection(verts_tri)
+	#scube1 = Poly3DCollection(verts_tri)
 
-	scube1.set_edgecolor(edge_c)
-	scube1.set_linewidth(edge_w)
-	scube1.set_alpha(alpha)
-	scube1.set_facecolor(color2)
+	#scube1.set_edgecolor(edge_c)#
+	#scube1.set_linewidth(edge_w)
+	#scube1.set_alpha(alpha)
+	#scube1.set_facecolor(color2)
+
+	#all_cube = scube + scube1
+
+	#for c in all_cube:
+	#ax.add_collection3d(scube1)
 
 	ax.add_collection3d(scube)
-	ax.add_collection3d(scube1)
