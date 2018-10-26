effb64f056f58cd20c26c0e3587ae5506fee4cf0
Spencer Riley <spencerriley620@gmail.com>
2018-10-26 16:33:54 -0600

    Update cuboctahedron.py

--- src/Current Models/Archimedean/cuboctahedron.py
+++ src/Current Models/Archimedean/cuboctahedron.py
@@ -6,6 +6,28 @@ from matplotlib import *
 from numpy import *
 from mpl_toolkits.mplot3d.art3d import *
 from matplotlib.animation import *
+from matplotlib.text import Annotation
+from mpl_toolkits.mplot3d.proj3d import proj_transform
+
+# Used to generate the labels
+class Annotation3D(Annotation):
+    '''Annotate the point xyz with text s'''
+
+    def __init__(self, s, xyz, *args, **kwargs):
+        Annotation.__init__(self,s, xy=(0,0), *args, **kwargs)
+        self._verts3d = xyz
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.xy=(xs,ys)
+        Annotation.draw(self, renderer)
+
+def annotate3D(ax, s, *args, **kwargs):
+    '''add anotation text s to to Axes3d ax'''
+
+    tag = Annotation3D(s, *args, **kwargs)
+    ax.add_artist(tag)
 
 name = "Cuboctahedron"
 
@@ -38,10 +60,11 @@ def shape(fig, alpha, color, edge_c, edge_w, grid, color2):
 		Z[i, :] = dot(points[i, :], P)
 
 	# Figure Properties
+	#fig = plt.figure(figsize=(8,8))
 	ax = p3.Axes3D(fig)
 	ax.set_facecolor('black')
 
-	plt.axis("on")
+	plt.axis(grid)
 	plt.axis('equal')
 
 	ax.set_xlim(-1, 1)
@@ -68,18 +91,34 @@ def shape(fig, alpha, color, edge_c, edge_w, grid, color2):
 
 	cuboc = Poly3DCollection(verts_cuboc)
 
-	cuboc.set_edgecolor(edge_c)
-	cuboc.set_linewidth(edge_w)
-	cuboc.set_alpha(alpha)
-	cuboc.set_facecolor(color)
+	cuboc.set_edgecolor("white")
+	cuboc.set_linewidth(1)
+	cuboc.set_alpha(0.5)
+	cuboc.set_facecolor("deepskyblue")
 
 	cuboc1 = Poly3DCollection(cuboc_three)
 
-	cuboc1.set_edgecolor(edge_c)
-	cuboc1.set_linewidth(edge_w)
-	cuboc1.set_alpha(alpha)
-	cuboc1.set_facecolor(color2)
+	cuboc1.set_edgecolor("white")
+	cuboc1.set_linewidth(1)
+	cuboc1.set_alpha(0.5)
+	cuboc1.set_facecolor("deepskyblue")
 
 	# Plot Surfaces
 	ax.add_collection3d(cuboc)
 	ax.add_collection3d(cuboc1)
+
+	if grid == "on":
+		# Produces the labels and arrows of the Hexagonal Face
+		for j, xyz_ in enumerate(points):
+		   hex = annotate3D(ax,
+				       s                    =   (j),
+				       xyz                  =   xyz_,
+				       fontsize             =   13,
+				       xytext               =   (-3,3),
+				       textcoords           =   'offset points',
+				       horizontalalignment  =   'right',
+				       verticalalignment    =   'bottom',
+				       arrowprops           =   dict(arrowstyle='<-', connectionstyle="arc3, rad=0.5")
+				            )
+		if grid == "off":
+			ax.clear()
